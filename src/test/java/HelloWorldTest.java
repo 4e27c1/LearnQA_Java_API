223def45219b80329a290fb41c53c373dcc8fc5d
avakchurin <4e27c1@gmail.com>
2022-12-11 23:25:32 +0300

    Homework Ex.5 second message from https://playground.learnqa.ru/api/get_json_homework

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,16 +1,29 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
     @Test
-    public void HomeworkEx4() {
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
-        
+    public void HomeworkEx5() {
+
+
+        JsonPath response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String message = response.get("messages[1].message");
+        System.out.println(message);
+
+
+
+
+
 
     }
 }
