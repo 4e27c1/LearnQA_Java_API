605d2207e60cff8281eff2e3b989603f240cc14b
avakchurin <4e27c1@gmail.com>
2022-12-25 23:04:54 +0300

    Homework ex11

--- src/test/java/tests/ex10.java
+++ src/test/java/tests/ex10.java
@@ -1,18 +1,33 @@
 package tests;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ex10 {
     @Test
-    public void testLenth(){
-        String str = "jnsdjkf";
-        int l = str.length();
 
+    public void testHWCookie(){
 
-       assertTrue(l>=15, "Lenth is less than 15");
-    }
+        Response response = RestAssured
+                .given()
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+
+                .andReturn();
+
+        String cookie_value = response.getCookie("Homework");
+        System.out.println("cookie value is " + cookie_value);
+
+         assertTrue(cookie_value!=null, "Cookie value is null");
+
+             }
 
 }
