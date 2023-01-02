e66b1eb24f24877d972734714043d856a0f2f61d
avakchurin <4e27c1@gmail.com>
2023-01-02 12:54:35 +0300

    Homework ex12

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -6,10 +6,37 @@ import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
-    public static void asserJsonByName(Response Response, String name, int expectedValue){
+    public static void asserJsonByName(Response Response, String name, int expectedValue) {
         Response.then().assertThat().body("$", hasKey(name));
         int value = Response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "Json value is not equal to expected value");
 
     }
+    public static void asserJsonByName(Response Response, String name, String expectedValue) {
+        Response.then().assertThat().body("$", hasKey(name));
+        String value = Response.jsonPath().getString(name);
+        assertEquals(expectedValue, value, "Json value is not equal to expected value");
+
+    }
+
+    public static void assertResponseTextEquals(Response Response, String expectedAnswer) {
+        assertEquals(
+                expectedAnswer,
+                Response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals(Response Response, int expectedStatusCode) {
+        assertEquals(
+                expectedStatusCode,
+                Response.statusCode(),
+                "Response StatusCode is not as expected"
+        );
+    }
+
+    public static void assertJsonHasKey(Response Response, String expectedFieldName){
+        Response.then().assertThat().body("$",hasKey(expectedFieldName));
+    }
+
 }
